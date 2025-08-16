2267bcaf6dea87a950fab3092e74664ca2807e97
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-08-16 15:17:19 -0500

    :sparkles: create run main web api

---
+++ app/web_api.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import pandas as pd
+import joblib
+from dto.log_entry import LogEntry
+from pathlib import Path
+
+app = FastAPI()
+
+model = joblib.load(Path(__file__).parent.parent / 'model/isolation_forest_model.pkl')
+
+
+@app.post("/analyze")
+def analyze(logs: list[LogEntry]):
+    log_dicts = [log.to_dict() for log in logs]
+    df_logs = pd.DataFrame(log_dicts)
+
+    features = df_logs[['Country', 'Region', 'City', 'Device Type', 'Is Attack IP', 'Is Account Takeover']]
+
+    features['Country'] = features['Country'].astype('category').cat.codes
+    features['Region'] = features['Region'].astype('category').cat.codes
+    features['City'] = features['City'].astype('category').cat.codes
+    features['Device Type'] = features['Device Type'].astype('category').cat.codes
+
+    predictions = model.predict(features)
+
+    result = {"message": "Análisis completado", "predictions": predictions.tolist()}
+    return result
+

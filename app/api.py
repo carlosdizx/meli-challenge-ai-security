875d29966b218291292d475f28f0eddb11f4da77
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-08-17 18:26:17 -0500

    :sparkles: set try and create get endpoint

--- app/api.py
+++ app/api.py
@@ -6,23 +6,30 @@ from pathlib import Path
 
 app = FastAPI()
 
-model = joblib.load(Path(__file__).parent.parent / 'model/isolation_forest_model.pkl')
+model = joblib.load(Path(__file__).parent.parent / 'model/isolation_forest.pkl')
+
+
+@app.get("/greeting")
+def read_root():
+    return {"Hello": "World"}
 
 
 @app.post("/analyze")
 def analyze(logs: list[LogEntry]):
-    log_dicts = [log.to_dict() for log in logs]
-    df_logs = pd.DataFrame(log_dicts)
+    try:
+        log_dicts = [log.to_dict() for log in logs]
+        df_logs = pd.DataFrame(log_dicts)
 
-    features = df_logs[['Country', 'Region', 'City', 'Device Type', 'Is Attack IP', 'Is Account Takeover']]
+        features = df_logs[['Country', 'Region', 'City', 'Device Type', 'Is Attack IP']]
 
-    features['Country'] = features['Country'].astype('category').cat.codes
-    features['Region'] = features['Region'].astype('category').cat.codes
-    features['City'] = features['City'].astype('category').cat.codes
-    features['Device Type'] = features['Device Type'].astype('category').cat.codes
+        features['Country'] = features['Country'].astype('category').cat.codes
+        features['Region'] = features['Region'].astype('category').cat.codes
+        features['City'] = features['City'].astype('category').cat.codes
+        features['Device Type'] = features['Device Type'].astype('category').cat.codes
 
-    predictions = model.predict(features)
+        predictions = model.predict(features)
 
-    result = {"message": "Análisis completado", "predictions": predictions.tolist()}
-    return result
-
+        result = {"message": "Análisis completado", "predictions": predictions.tolist()}
+        return result
+    except Exception as e:
+        raise Exception(e)

c366d0d8aed94a59093aabbaeb3d31b621ca0093
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-08-17 19:32:08 -0500

    :sparkles: change columns to export

--- scripts/load_dataset.py
+++ scripts/load_dataset.py
@@ -3,6 +3,7 @@ import pandas as pd
 from kagglehub import dataset_download
 import tomli
 from pathlib import Path
+import ipaddress
 
 # 2. Leer secrets.toml
 secrets_path = Path(__file__).parent.parent / '.streamlit' / 'secrets.toml'
@@ -17,20 +18,23 @@ path = dataset_download("dasgroup/rba-dataset")
 df = pd.read_csv(f"{path}/rba-dataset.csv", nrows=int(rows))
 
 # 4.Limpiar el dataset
-df.drop(columns=['User ID', 'User Agent String', 'Browser Name and Version', 'OS Name and Version',
-                 'Round-Trip Time [ms]', 'ASN', "Login Timestamp", "IP Address", "Region", "City"], inplace=True)
-
-df['Device Type'] = df['Device Type'].fillna('unknown')
+df.drop(columns=['index', 'Round-Trip Time [ms]', "Login Timestamp", "Region", "City"],
+        inplace=True)
 
 # 5. Transformación de datos, strings a numéricos
 
+df['IP Address'] = df['IP Address'].apply(lambda x: int(ipaddress.ip_address(x)))
+df['Device Type'] = df['Device Type'].fillna('unknown')
+
+df['User Agent String'], _ = pd.factorize(df['User Agent String'])
+df['Browser Name and Version'], _ = pd.factorize(df['Browser Name and Version'])
+df['OS Name and Version'], _ = pd.factorize(df['OS Name and Version'])
+
 categorical_cols = df.select_dtypes(include=['object']).columns
 
-print(df.head(5))
-
-# for col in categorical_cols:
-#     df[col] = df[col].astype('category')
-#     df[col] = df[col].cat.codes
+for col in categorical_cols:
+    df[col] = df[col].astype('category')
+    df[col] = df[col].cat.codes
 
 # 6.Exportar el dataset limpio a un archivo CSV
 output_path = Path(__file__).parent.parent / 'data' / 'dataset.csv'

a56c9b1ad34993b32051f3334e4f505c6daa0384
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-08-19 16:50:46 -0500

    :sparkles: transform data

--- app/routers/analyze.py
+++ app/routers/analyze.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import pandas as pd
+
+from app.services.model_service import model_service
 from dto.log_entry import LogEntry
 
 router = APIRouter()
 
+FEATURE_KEYMAP = {
+    "country": "Country",
+    "asn": "ASN",
+    "user_agent_string": "User Agent String",
+    "browser_name_and_version": "Browser Name and Version",
+    "os_name_and_version": "OS Name and Version",
+    "device_type": "Device Type",
+    "login_successful": "Login Successful",
+}
+
 
 @router.post("/")
 def analyze(logs: list[LogEntry]):
-    return {
-        "received": len(logs),
-        "threat_detected": False,
-        "scores": [],
-        "suggested_action": "allow",
-    }
+    log_dicts = [log.to_dict() for log in logs]
+
+    try:
+        return log_dicts
+    except Exception as e:
+        print(e)
+        raise e

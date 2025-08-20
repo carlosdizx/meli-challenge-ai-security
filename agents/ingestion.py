3948fcd1224ab00250e03012f098be3ae874826b
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-08-19 23:52:24 -0500

    :fre: remove unnecessary code

--- agents/ingestion.py
+++ agents/ingestion.py
@@ -3,16 +3,6 @@ import pandas as pd
 from dto.log_entry import LogEntry
 from graph.pipeline_state import PipelineState
 
-FEATURE_KEYMAP = {
-    "country": "Country",
-    "asn": "ASN",
-    "user_agent_string": "User Agent String",
-    "browser_name_and_version": "Browser Name and Version",
-    "os_name_and_version": "OS Name and Version",
-    "device_type": "Device Type",
-    "login_successful": "Login Successful",
-}
-
 
 def ingest(state: PipelineState) -> PipelineState:
     logs: list[LogEntry] = state["logs_input"]

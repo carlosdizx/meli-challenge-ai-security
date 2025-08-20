214acd15f62b54713d1ed5f4bed22c9675ab95dc
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-08-20 00:25:42 -0500

    :truck: move files

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.routers.health import router as health_router
-from app.routers.analyze import router as analyze_router
+from data.routers.health import router as health_router
+from data.routers.analyze import router as analyze_router
 
 app = FastAPI(title="Security and AI for Risk-Based Authentication", version="1.0.0")
 

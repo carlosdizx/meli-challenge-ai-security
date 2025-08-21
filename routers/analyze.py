c392ee7ec33902d98762970366f5fea85c436891
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2025-08-20 22:59:56 -0500

    :fire: remove unnecessary data

--- routers/analyze.py
+++ routers/analyze.py
@@ -15,16 +15,6 @@ if not model_service.is_loaded():
 
 router = APIRouter()
 
-FEATURE_KEYMAP = {
-    "country": "Country",
-    "asn": "ASN",
-    "user_agent_string": "User Agent String",
-    "browser_name_and_version": "Browser Name and Version",
-    "os_name_and_version": "OS Name and Version",
-    "device_type": "Device Type",
-    "login_successful": "Login Successful",
-}
-
 
 @router.post("/")
 def analyze(logs: list[LogEntry]):
